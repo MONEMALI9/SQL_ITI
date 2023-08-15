fbc9d243a76fe7be8a9566183c8f2291dda89418
Abdelmonem Elmongy <100364869+MONEMALI9@users.noreply.github.com>
2023-08-15 20:29:10 +0300

    Add files via upload

--- sql/SQLQuaryDAY10.sql
+++ sql/SQLQuaryDAY10.sql
@@ -1,28 +1,76 @@
---1. Create a cursor for Employee table that increases Employee salary by 10% if Salary <3000 and increases it by 20% if Salary >=3000. Use company DB
-declare c1 Cursor
-for Select st_id,st_fname
-	from Student
-	where st_address='cairo'
-for read only    --update
+--Cursors task
+--1.Create a cursor for Employee table that increases Employee salary by 10% if Salary <3000
+--and increases it by 20% if Salary >=3000. Use company DB
 
+declare s_cur1 cursor
+	for select ssn, concat(Fname,' ',Lname) full_name ,Salary from Employee
+	for update
 
---2. Display Department name with its manager name using cursor. Use ITI DB
-declare @id int,@name varchar(20)
-open c1
-fetch c1 into @id,@name
-while @@FETCH_STATUS=0
+declare @id int
+declare @name varchar(50)
+declare @s int
+open s_cur1 
+fetch s_cur1 into @id,@name,@s
+begin
+	While @@fetch_status=0
 	begin
-		Select @id,@name
-		fetch c1 into @id,@name			
+		if @s<3000
+			begin
+update Employee	set Salary= @s*1.10 where current of s_cur1 
+		    end
+		else if @s >=3000
+update Employee	set Salary= @s*1.20 where current of s_cur1 
+fetch s_cur1 into @id,@name,@s
+
 	end
-close c1
-deallocate c1
+end
+close s_cur1
+deallocate s_cur1
 
 
---3. Try to display all students first name in one cell separated by comma. Using Cursor
-select st_fname
-from student
-where st_fname is not NULL
+--2.Display Department name with its manager name using cursor. Use ITI DB
+declare s_cur2 cursor
+	for select d.Dept_Name , i.Ins_Name
+	from Instructor i join Department d
+	on i.Ins_Id=d.Dept_Manager
+	for read only				--(other option is )update
+
+
+declare @Dept_Name nvarchar(50)
+declare @manager_name nvarchar(50)
+open s_cur2
+fetch s_cur2 into @dept_name,@manager_name			--put first id and name into vars
+begin
+	While @@fetch_status=0  --returns 0 success -- 1 failed  --2 no more rows to fetch
+	begin
+		select @Dept_Name,@manager_name
+		fetch s_cur2 into @Dept_Name,@manager_name		-- go to next row
+	end
+end
+close s_cur2
+deallocate s_cur2
+	
+
+--3.Try to display all students first name in one cell separated by comma. Using Cursor 
+declare s_cur3 cursor
+	for select st_fname from student
+	for read only				--(other option is )update
+
+declare @first nvarchar(50)
+declare @x nvarchar(50) =' ' 
+open s_cur3 
+fetch s_cur3 into @first			--put first id and name into vars
+begin
+	While @@fetch_status=0  --returns 0 success -- 1 failed  --2 no more rows to fetch
+	begin
+		set @first=@first+ ',' + @x
+		select @first
+		fetch s_cur3 into @x	
+	end
+end
+close s_cur3
+deallocate s_cur3
+	
 
 
 --One Cell [ahmed,Amr,Mona..........]
@@ -43,166 +91,3 @@ while @@FETCH_STATUS=0
 Select @allnames
 close c1
 deallocate c1
-
-
---5. Use import export wizard to display students data (ITI DB) in excel sheet
-declare c1 cursor
-for Select salary
-	from Instructor
-for update    --DML
-declare @sal int
-open c1
-fetch c1 into @sal
-while @@FETCH_STATUS=0
-	begin
-		if @sal>=3000
-			update Instructor	
-				set salary=@sal*1.20
-			where current of c1
-		else
-			update Instructor
-				set salary=@sal*1.10
-			where current of c1
-		fetch c1 into @sal
-	end
-close c1
-deallocate c1
--------------------------------------
-
---6. Try to generate script from DB ITI that describes all tables and views in this DB
-declare c1 Cursor
-for select st_fname
-    from student
-for read only
-
---7. Create Snapshot for CompanyDB.
-declare @name varchar(20),@counter int=0,@flag int=0
-open c1
-fetch c1 into @name
-while @@FETCH_STATUS=0
-	begin
-		if @name='ahmed'
-			begin
-				set @flag=1 
-			end
-		if @name='amr'
-			begin
-				if @flag=1
-					begin
-						set @counter+=1
-						set @flag=0
-					end 
-			end
-		fetch c1 into @name
-	end
-select @counter
-close c1
-deallocate c1
--------------------------------------------------------------
-
---8. Create job for backup ITI DB every day at 12:00PM
-backup database ITI
-to disk='d:\iti_db.bak'
-
--------------------------------------------------------------
---9. Create a sequence object that allow values from 1 to 10 without cycling in a specific column and test it (self study)
-CREATE TABLE dbo.T1 ( column_1 int, column_2 VARCHAR(30),
-					column_3 int IDENTITY primary key);
-GO
-
-SELECT * FROM T1
-
-delete from T1  where Column_3 between 2 and 20
-
-
-
-INSERT T1 VALUES (100,'ahmed');
-
-INSERT T1 (column_2) VALUES ('Row #2');
-GO
-SET IDENTITY_INSERT T1 ON;
-SET IDENTITY_Insert T1 off;
-GO
-
-
-INSERT INTO T1 (column_3,column_1,column_2)  VALUES 
-(7,777, 'eman');
-
-truncate table t1
-
-GO
-SELECT column_1, column_2,column_3
-FROM T1;
-
-drop table T1
-
-dbcc checkident(T1,RESEED,3)
-
-select @@identity
-
-SELECT IDENT_CURRENT('t1')
-------------------------------------------------------
---types of insert statement
--------------------------
---simple insert
---insert constructor
---insert based on select
---insert based on execute
---bulk insert
-bulk insert emp
-from 'd:\mydata.txt'
-with (fieldterminator=',')
-------------------------------------------------------
---Snapshot (read only DB)
----------------
-create Database itisnap
-on
-(
-name='ITI',
-filename='d:\itisnap.ss'
-)
-as snapshot of iti
-
-
-select * from student
-
-
-restore database iti
-from database_snapshot='itisnap'	--snapshot name in object explorer
-
---Cursor
---backup
---snapshot
-
---SQLCLR
------------Function
------------New data type    [class struct]
-
-sp_configure 'clr_enable',1
-go
-reconfigure
-
-
-select dbo.sum2Int(3,5)
-
-select dbo.sum2Int(st_id,st_age)
-from student
-
-create table shapes
-(
-_id int,
-_desc varchar(20),
-_coords Circle
-)
-
---10,20,30
-select _desc
-from shapes
-where _Coords.x>=10
-
-select _desc
-from shapes
-
-
-
-
